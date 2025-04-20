3f5155d5959a98eb7b16deaf2988befe09641010
intan <intan@LAPTOP-FROGL7J3>
2025-04-20 17:42:29 +0700

    first commit

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+import java.sql.*;
+
+/**
+ *
+ * @author intan
+ */
+public class koneksi {
+    private Connection koneksi;
+    public Connection connect(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Berhasil Konek");
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println("gagal koneksi"+ex);
+        }
+        String url="jdbc:mysql://localhost/penjualan_new";
+        try{
+            koneksi=DriverManager.getConnection(url,"root","");
+            System.out.println("Berhasil koneksi database");
+        }
+        catch(SQLException ex){
+            System.out.println("gagal koneksi database"+ ex);
+        }
+        return koneksi;
+    }
+}
